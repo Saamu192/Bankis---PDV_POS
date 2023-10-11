89eff5317c654d1e1e0b5b8cdf702b61ba2458f8
Samuel <dev.19@pbhub.com.br>
2023-10-10 23:43:25 -0300

    feat index

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
 import "dotenv/config";
+import { exec } from "node:child_process";
 
 const PORT = process.env.PORT || 3000;
 
@@ -9,8 +10,12 @@ const app = express();
 
 const swaggerDocument = YAML.load("./swagger.yml");
 
-app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, () => {
   console.log(`Server Running at PORT ${PORT} and PID ${process.pid}`);
 });
+
+(() => {
+  exec("npm run start");
+})();
